87e6da1d53d55430bce6ac340948fa1796b62c21
Swasthikkamath <118376716+Swasthikkamath@users.noreply.github.com>
2026-06-25 00:23:43 +0530

    Update axi4_slave_monitor_bfm.sv

--- src/hdl_top/slave_agent_bfm/axi4_slave_monitor_bfm.sv
+++ src/hdl_top/slave_agent_bfm/axi4_slave_monitor_bfm.sv
@@ -165,7 +165,7 @@ interface axi4_slave_monitor_bfm(input aclk, input aresetn,
       `uvm_info("FROM SLAVE MON BFM",$sformatf("Inside while loop of write response sample"),UVM_HIGH)
     end    
     `uvm_info("FROM SLAVE MON BFM",$sformatf("after while loop of write response "),UVM_HIGH)
-   
+    @(slaveMonCb);
     $display("sampled bid is %d",slaveMonCb.bid);
     req.bid      = slaveMonCb.bid;
     req.bresp    = slaveMonCb.bresp;  
